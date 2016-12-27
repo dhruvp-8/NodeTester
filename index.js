6cb3c92e4758b7fe4be5362ce9b95c8d0a08baab
dhruvp-8 <dhruvpatel5738@gmail.com>
2016-12-27 23:40:06 +0530

    Initial Commit 10

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -15,45 +16,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-navigator.webkitGetUserMedia({
-    video: true,
-    audio: false
-}, function(stream){
-    var peer = new Peer({
-        initiator: location.hash === '#init',
-        trickle: false,
-        stream: stream
-    })
-
-    peer.on('signal', function(data){
-        document.getElementById('yourId').value = JSON.stringify(data)
-    })
-
-    document.getElementById('connect').addEventListener('click', function
-    () {
-        var otherId = JSON.parse(document.getElementById('otherId').value)
-        peer.signal(otherId)
-    })
-
-    document.getElementById('send').addEventListener('click', function
-    () {
-        var yourMessage = document.getElementById('yourMessage').value
-        peer.send(yourMessage)
-    })
-
-    peer.on('data', function(data){
-        document.getElementById('messages').textContent += data + '\n'
-    })
-
-    peer.on('stream', function(stream){
-        var video = document.createElement('video')
-        document.body.appendChild(video)
-
-        video.src = window.URL.createObjectURL(stream)
-        video.play()
-    })
-
-}, function(err){
-        console.error(err)
-})
