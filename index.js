02b71dac16908b33ec08e1867019dec07f1b2d3e
dhruvp-8 <dhruvpatel5738@gmail.com>
2016-12-27 22:53:27 +0530

    Initial Commit 8

--- index.js
+++ index.js
@@ -16,3 +16,46 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var Peer = require('simple-peer')
+navigator.webkitGetUserMedia({
+    video: true,
+    audio: false
+}, function(stream){
+    var peer = new Peer({
+        initiator: location.hash === '#init',
+        trickle: false,
+        stream: stream
+    })
+
+    peer.on('signal', function(data){
+        document.getElementById('yourId').value = JSON.stringify(data)
+    })
+
+    document.getElementById('connect').addEventListener('click', function
+    () {
+        var otherId = JSON.parse(document.getElementById('otherId').value)
+        peer.signal(otherId)
+    })
+
+    document.getElementById('send').addEventListener('click', function
+    () {
+        var yourMessage = document.getElementById('yourMessage').value
+        peer.send(yourMessage)
+    })
+
+    peer.on('data', function(data){
+        document.getElementById('messages').textContent += data + '\n'
+    })
+
+    peer.on('stream', function(stream){
+        var video = document.createElement('video')
+        document.body.appendChild(video)
+
+        video.src = window.URL.createObjectURL(stream)
+        video.play()
+    })
+
+}, function(err){
+        console.error(err)
+})
